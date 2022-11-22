e08d9521a5eea4c98fa122b11a9aec5691af7aa3
hemarina <hemarina@microsoft.com>
2022-11-22 09:40:27 -0800

    change author

--- website/src/data/users.tsx
+++ website/src/data/users.tsx
@@ -235,8 +235,8 @@ const Users: User[] = [
       'A restaurants review web app that uses whitenoise for static assets and PostGreSQL for the database. Ready for deployment on App Service with a PostGreSQL Flexible Server inside a VNet.',
       preview: require('./images/msdocs-django-postgresql-sample-app-azd.png'),
     website: 'https://github.com/pamelafox',
-    author: 'Pamela Fox',
-    source: 'https://github.com/pamelafox/msdocs-django-postgresql-sample-app-azd',
+    author: 'Azure Content Team',
+    source: 'https://github.com/Azure-Samples/msdocs-django-postgresql-sample-app',
     tags: ['python','django', 'webapps', 'azuredb-postgreSQL', 'appservice','vnets'],
   },
 

73931b3482ebe1b47c18fd86ff7a1cca5437e051
hemarina <hemarina@microsoft.com>
2023-01-05 14:05:48 -0800

    add template

--- website/src/data/tags.tsx
+++ website/src/data/tags.tsx
@@ -94,6 +94,7 @@ export type TagType =
 | 'openai'
 | 'azureai'
 | 'flask'
+| 'apim'
 ;
 
 
@@ -341,6 +342,11 @@ export const Tags: {[type in TagType]: Tag} = {
     azureai: {
         label: 'Azure AI Service',
         description: 'Template architecture uses Azure AI Service',
+        color: 'hotpink',
+    },
+    apim: {
+        label: 'Azure API Management',
+        description: 'Template architecture uses Azure API Management',
         color: 'hotpink',
     },
 };

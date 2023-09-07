3c6a33e66a3cdfd3973133535bf5fbc63d632a1c
hemarina <hemarina@microsoft.com>
2023-09-06 20:11:53 -0700

    adjust due to suggestions

--- website/src/components/gallery/ShowcaseCard/index.tsx
+++ website/src/components/gallery/ShowcaseCard/index.tsx
@@ -132,6 +132,7 @@ const useStyles = makeStyles({
     height: "368px",
     maxWidth: "100%",
     maxHeight: "100%",
+    minWidth:'300px',
   },
   text: {
     color: "#606060",
@@ -349,7 +350,7 @@ function closeCard(parentDiv) {
 export function ShowcaseContributionCard(): React.ReactElement {
   const styles = useStyles();
   if (localStorage.getItem('contributionCardDisplay')){
-    return <></>;
+    return null;
   }
     return (
       <Card className={styles.card} id="contributionCard">
@@ -378,6 +379,8 @@ export function ShowcaseContributionCard(): React.ReactElement {
             color: "#242424",
             fontSize: "24px",
             fontFamily: '"Segoe UI-Semibold", Helvetica',
+            fontWeight: "550",
+            height:'0px',
           }}
         >
           See your template here!
@@ -404,7 +407,7 @@ export function ShowcaseContributionCard(): React.ReactElement {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
         </div>
-        <div style={{ display: "flex" }}>
+        <div style={{ display: "flex", paddingTop: "10px" }}>
           <Button
             size="small"
             style={{
@@ -415,6 +418,7 @@ export function ShowcaseContributionCard(): React.ReactElement {
               backgroundColor: "#6656d1",
               height: "32px",
               whiteSpace: "nowrap",
+              fontWeight: "550",
             }}
           >
             Submit a template
@@ -429,6 +433,8 @@ export function ShowcaseContributionCard(): React.ReactElement {
               fontSize: "14px",
               height: "32px",
               whiteSpace: "nowrap",
+              fontWeight: "550",
+              paddingLeft:'10px'
             }}
           >
             Request a template

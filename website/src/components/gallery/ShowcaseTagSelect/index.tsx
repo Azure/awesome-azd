8c0df44c5e9f8c4802c06693d1b440c44d257bdf
hemarina <hemarina@microsoft.com>
2023-09-21 15:28:31 -0700

    minor fix on merge

--- website/src/components/gallery/ShowcaseTagSelect/index.tsx
+++ website/src/components/gallery/ShowcaseTagSelect/index.tsx
@@ -22,8 +22,6 @@
   
 import { Checkbox } from "@fluentui/react-components";
   
-import { Checkbox } from "@fluentui/react-components";
-  
   const TagQueryStringKey = 'tags';
   
   export function readSearchTags(search: string): TagType[] {

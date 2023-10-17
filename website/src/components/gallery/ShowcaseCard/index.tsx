16f055c7277c54a305c7fbe80aa87b6812f5cc4f
hemarina <hemarina@microsoft.com>
2023-10-17 14:44:53 -0700

    correct copy right

--- website/src/components/gallery/ShowcaseCard/index.tsx
+++ website/src/components/gallery/ShowcaseCard/index.tsx
@@ -1,10 +1,6 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+/* Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
  */
-
 import React from "react";
 import styles from "./styles.module.css";
 import { Tag, Tags, type User, type TagType } from "../../../data/tags";

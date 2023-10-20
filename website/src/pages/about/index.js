f965891ba6c0b8fb50857720c0a101d90c544122
hemarina <hemarina@microsoft.com>
2023-10-20 14:50:50 -0700

    add background on about

--- website/src/pages/about/index.js
+++ website/src/pages/about/index.js
@@ -1,25 +1,31 @@
 import React from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={styles.heroBanner}>
-      <div>
-        <iframe
-          width="773"
-          height="435"
-          src="https://www.youtube.com/embed/9z3PiHSCcYs?si=F1yKpoiOQnzb4o-K"
-          title="Azure Developer CLI: GitHub to cloud in minutes - Universe 2022"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
+        <img
+          src={useBaseUrl("/img/coverBackground.png")}
+          className={styles.cover}
+          onError={({ currentTarget }) => {
+            currentTarget.style.display = "none";
+          }}
+          alt=""
+        />
+        <div className={styles.description}>
+          <iframe
+            className={styles.video}
+            height="340"
+            src="https://www.youtube.com/embed/9z3PiHSCcYs?si=F1yKpoiOQnzb4o-K"
+            title="Azure Developer CLI: GitHub to cloud in minutes - Universe 2022"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
       </div>
     </header>
   );

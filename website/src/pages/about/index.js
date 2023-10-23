9b0da1239d16c2faa17c8c5eb701c038f3747a91
hemarina <hemarina@microsoft.com>
2023-10-23 16:00:09 -0700

    add hero bar part 1

--- website/src/pages/about/index.js
+++ website/src/pages/about/index.js
@@ -3,30 +3,51 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import useBaseUrl from "@docusaurus/useBaseUrl";
+import { Text } from "@fluentui/react-components";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
   return (
     <header className={styles.heroBanner}>
-        <img
-          src={useBaseUrl("/img/coverBackground.png")}
-          className={styles.cover}
-          onError={({ currentTarget }) => {
-            currentTarget.style.display = "none";
-          }}
-          alt=""
-        />
+      <img
+        src={useBaseUrl("/img/coverBackground.png")}
+        className={styles.cover}
+        onError={({ currentTarget }) => {
+          currentTarget.style.display = "none";
+        }}
+        alt=""
+      />
+      <div className={styles.section}>
+        {/* <div className={styles.description}>
+          <div className={styles.title}>
+            <div>Accelerate your journey to </div>
+            <div> the cloud with azd</div>
+          </div>
+          <div className={styles.content}>
+            <div>Azure Developer CLI (azd) is an open-source</div>
+            <div>tool that accelerates your application’s journey</div>
+            <div>from local development to Azure</div>
+          </div>
+        </div> */}
         <div className={styles.description}>
-          {/* <iframe
+          <div className={styles.title}>
+            Accelerate your journey to the cloud with azd
+          </div>
+          <div className={styles.content}>
+            Azure Developer CLI (azd) is an open-source tool that accelerates
+            your application’s journey from local development to Azure
+          </div>
+        </div>
+        <div>
+          <iframe
             className={styles.video}
-            height="340"
             src="https://www.youtube.com/embed/9z3PiHSCcYs?si=F1yKpoiOQnzb4o-K"
             title="Azure Developer CLI: GitHub to cloud in minutes - Universe 2022"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
-          ></iframe> */}
-          Hello
+          ></iframe>
+        </div>
       </div>
     </header>
   );

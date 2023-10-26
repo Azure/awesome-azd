257db6525bec31d90ad492e0a3737338ea2f68fd
hemarina <hemarina@microsoft.com>
2023-10-25 20:17:25 -0700

    finish about page

--- website/src/pages/about/index.js
+++ website/src/pages/about/index.js
@@ -3,7 +3,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import useBaseUrl from "@docusaurus/useBaseUrl";
-import { Text } from "@fluentui/react-components";
+import { FluentProvider, Text, teamsLightTheme } from "@fluentui/react-components";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
@@ -43,9 +43,8 @@ function HomepageHeader() {
             className={styles.video}
             src="https://www.youtube.com/embed/9z3PiHSCcYs?si=F1yKpoiOQnzb4o-K"
             title="Azure Developer CLI: GitHub to cloud in minutes - Universe 2022"
-            frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </div>
       </div>
@@ -61,9 +60,9 @@ export default function Home() {
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
-      <main>
+      <FluentProvider theme={teamsLightTheme}>
         <HomepageFeatures />
-      </main>
+      </FluentProvider>
     </Layout>
   );
 }
